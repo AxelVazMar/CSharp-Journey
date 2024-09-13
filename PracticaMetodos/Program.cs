25927ff6f24e3e15ca15ba875b37b55e551729a3
Axel Vazquez <axelvazmar0@gmail.com>
2024-09-12 20:35:00 -0600

    Initial Commit in my Journey on C#

---
+++ PracticaMetodos/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        Console.WriteLine(divisionNumeros(18, 7));
+        Console.WriteLine(divisionNumeros2(3, 5));
+    }
+
+    static double divisionNumeros(double num1, int num2) // Un double con un int siempre dará double
+    {
+        // Cuando un programa lee un return, este automaticamente se sale del método
+        return num1 / num2;
+    }
+
+    // Otra manera de declarar métodos cuando el cuerpod el método tiene una sola línea de código
+    static double divisionNumeros2(double num1, double num2) => num1 / num2;
+}
