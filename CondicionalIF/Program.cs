a9c5655e01fb46d607f88c93416532054cb20fb9
Axel Vazquez <axelvazmar0@gmail.com>
2024-09-14 12:44:56 -0600

    Usando if-else y el operador lódigo &&

--- CondicionalIF/Program.cs
+++ CondicionalIF/Program.cs
@@ -1,19 +1,56 @@
-﻿namespace CondicionalIF
+﻿
+namespace CondicionalIF
 {
     class Program
     {
         static void Main(string[] args)
         {
 
-            int edad = 15;
+            int edad = 28;
 
             System.Console.WriteLine("Vamos a evaluar si eres mayor de edad");
 
             if (edad >= 18)
+            {
+                Console.WriteLine("Adelante puedes pasar porque eres mayor de edad");
+            }
 
-                System.Console.WriteLine("Adelante puedes pasar porque eres mayor de edad");
-                System.Console.WriteLine("Disfurta de tu estancia");
+            System.Console.WriteLine("\nVamos a evaluar si puedes conducir un vehiculo");
 
+            bool licencia = false;
+
+            /* Si un bloque de else o if solo tienen una línea código estos pueden ir sin llaves
+            pero lo mas norma es seguir el siguiente patrón
+            if(condición) línea_de_código
+            */
+
+            if (licencia) System.Console.WriteLine("Puedes conducir vehiculos");
+
+            else System.Console.WriteLine("Lo siento, pero no puedes conducir vehiculos");
+
+            Console.WriteLine("Aquí termina el programa");
+
+            // Usando la instrucción &&
+
+            Console.WriteLine("Vamos a evaluar si puedes conducir un vehículo");
+
+            Console.WriteLine("Introduce tu edad, por favor:");
+
+            int edad2 = Int32.Parse(Console.ReadLine());
+
+            System.Console.WriteLine("¿Tienes licencia?");
+
+            string licencia2 = Console.ReadLine();
+
+            if (edad2 >= 18 && licencia2 == "si")
+            {
+                Console.WriteLine("Puedes conducir vehículos");
+            }
+
+            else
+            {
+                Console.WriteLine("No puedes conducir vehículos");
+            }
 
         }
     }
--- CondicionalIF/obj/Debug/net8.0/CondicionalIF.AssemblyInfo.cs
+++ CondicionalIF/obj/Debug/net8.0/CondicionalIF.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CondicionalIF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81ce1cf2fd13714475559f60ab25c6cc133083ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+334a03515b531b310c146fd38fe966790d5f04e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("CondicionalIF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CondicionalIF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
