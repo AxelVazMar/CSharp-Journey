334a03515b531b310c146fd38fe966790d5f04e6
Axel Vazquez <axelvazmar0@gmail.com>
2024-09-13 23:54:43 -0600

    Introducción al condicional IF

---
+++ CondicionalIF/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace CondicionalIF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int edad = 15;
+
+            System.Console.WriteLine("Vamos a evaluar si eres mayor de edad");
+
+            if (edad >= 18)
+
+                System.Console.WriteLine("Adelante puedes pasar porque eres mayor de edad");
+                System.Console.WriteLine("Disfurta de tu estancia");
+
+
+        }
+    }
+}
