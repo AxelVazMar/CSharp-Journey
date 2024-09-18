cb9ee82317e89dc8037c99faab2ae8a8b11b15aa
Axel Vazquez <axelvazmar0@gmail.com>
2024-09-17 20:22:12 -0600

    Conociendo el bucle While en C#}

---
+++ BucleWhile/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("¿Quieres entrar al bucle While?");
+
+        string respuesta = Console.ReadLine();
+
+        while (respuesta != "no")
+        {
+            System.Console.WriteLine("Estás ejecutando el bucle While");
+
+            System.Console.WriteLine("Introduce nombre porfavor");
+
+            string nombre = Console.ReadLine();
+
+            Console.WriteLine($"Saldrás del bucle, {nombre} cunado respondas 'no' a la pregunta");
+
+            Console.WriteLine("¿Deseas repetir el bucle otra vez?");
+
+            respuesta = Console.ReadLine();
+        }
+
+        Console.WriteLine("Estás fuera del bucle");
+
+    }
+}
