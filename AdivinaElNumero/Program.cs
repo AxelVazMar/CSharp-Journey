11a5677d189ed34e54ab09d8a60b5ac88059492b
Axel Vazquez <axelvazmar0@gmail.com>
2024-09-17 20:22:48 -0600

    Ejercicio de adivinar el número secreto usando el bucle While

---
+++ AdivinaElNumero/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Vamos a adivinar el número");
+        Console.WriteLine("\nIngresa el número a adivinar\nSolo es permitido en un rango de 0 a 100");
+        int nAdivinar = int.Parse(Console.ReadLine());
+        Random numero = new Random();
+        int numeroAleatorio = numero.Next(0, 100);
+        int intento = 1;
+
+
+        while (nAdivinar != numeroAleatorio)
+        {
+            if (nAdivinar < numeroAleatorio)
+            {
+                System.Console.WriteLine($"El numero es mayor que {nAdivinar}");
+                nAdivinar = int.Parse(Console.ReadLine());
+            }
+            else if (nAdivinar > numeroAleatorio)
+            {
+                System.Console.WriteLine($"El número es menor que {nAdivinar}");
+                nAdivinar = int.Parse(Console.ReadLine());
+            }
+            
+            intento ++;
+        }
+
+        System.Console.WriteLine($"Felicidades has encontrado el número secreto \nTu número de intentos fué de: " + intento);
+    }
+}
