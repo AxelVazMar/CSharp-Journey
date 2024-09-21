e692697c02b8f0e83bfd301b6cf07a64906baff5
Axel Vazquez <axelvazmar0@gmail.com>
2024-09-21 10:54:29 -0600

    Introducción a la programación orientada a objetos en C#

---
+++ EjemplosPOO/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace EjemplosPOO
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Circulo miCirculo; // Creación de objeto de tipo círculo => Variable/objeto de tipo círculo
+            miCirculo = new Circulo(); // Iniciación de variable/objeto de tipo círculo => Instanciar una clase => Instanciación => Ejemplarización => Creación de ejemplar de clase
+            Console.WriteLine(miCirculo.CalculoArea(5));
+
+            Circulo miCirculo2 = new Circulo(); // Al igual que con las variables podemos iniciar un objeto en la misma línea
+            Console.WriteLine(miCirculo2.CalculoArea(9));
+            Console.WriteLine(miCirculo.CalculoArea(5));
+
+        }
+    }
+
+    class Circulo
+    {
+        const double pi = 3.1416; // Propiedad de la clase Círculo => Campos de clase
+
+        public double CalculoArea(int radio) // <= Método de clase => Comportamiento de la clase
+        {
+            return pi * radio * radio;
+        }
+    }
+}
