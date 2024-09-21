a691b7e4bb7378e98dbf41abc90b5e06c7ae5b50
Axel Vazquez <axelvazmar0@gmail.com>
2024-09-20 21:49:48 -0600

    Terminando la sección de expeciones y conociendo el bloque finally y sus usos

---
+++ BloqueFinally/Program.cs
@@ -0,0 +1,37 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        System.IO.StreamReader archivo = null;
+
+        try
+        {
+            string linea;
+
+            int contandor = 0;
+
+            string path = @"D:\C# Projects\C#-Journey\BloqueFinally\archivo.txt";
+            archivo = new System.IO.StreamReader(path);
+
+            while ((linea = archivo.ReadLine()) != null)
+            {
+                Console.WriteLine(linea);
+
+                contandor++;
+            }
+
+        }
+        catch (Exception e)
+        {
+            System.Console.WriteLine("Error con la lectura del archivo");
+        }
+        finally
+        {
+            if (archivo != null)
+            {
+                archivo.Close();
+                System.Console.WriteLine("Archivo cerrado");
+            }
+        }
+    }
+}
