f33c466b924c1c694f152504faa19ddd269b0314
Axel Vazquez <axelvazmar0@gmail.com>
2024-09-21 10:54:00 -0600

    Conociendo la propiedad Checked para manejo de errores

---
+++ UsoChecked/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace UsoChecked
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Checked no sirve con doubles o floats, solo funciona con ints y longs
+            int numero = int.MaxValue;
+            int resultado = checked(numero + 20);
+            System.Console.WriteLine(resultado);
+        }
+    }
+}
