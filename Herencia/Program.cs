6e7713db869bdad30f292ef9cf6bd5c1e34a0fcd
Axel Vazquez <axelvazmar0@gmail.com>
2024-10-10 10:22:56 -0600

    Conociendo las bases de la herencia y sus sintaxis

---
+++ Herencia/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+
+        Caballo Friezan = new Caballo();
+        Humano Juan = new Humano();
+        Gorila Copito = new Gorila();
+
+        
+        Console.WriteLine();
+
+    }
+}
+
+class Mamiferos
+{
+    public void Respirar()
+        {
+            Console.WriteLine("Soy capaz de respirar");
+        }
+
+    public void CuidarCrias()
+        {
+            Console.WriteLine("Cuido de mis crías hasta que se puedan valer por sí mismas");
+        }
+
+     }
+
+    class Caballo : Mamiferos
+    {
+        public void Galopar()
+        {
+            System.Console.WriteLine("Soy capaz de galopar");
+        }
+     }
+
+    class Humano : Mamiferos
+     {
+        public void Pensar()
+        {
+            System.Console.WriteLine("Soy capaz de pensar ¿?");
+        }
+     }
+
+    class Gorila : Mamiferos
+     {
+        public void Trepar()
+        {
+            Console.WriteLine("Soy capaz de trepar");
+        }
+     }
