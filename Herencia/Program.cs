6063c067a49124efa96e58d0687466211d480ee0
Axel Vazquez <axelvazmar0@gmail.com>
2024-10-14 09:22:02 -0600

    Conociendo la palabra base en la herencia

--- Herencia/Program.cs
+++ Herencia/Program.cs
@@ -5,18 +5,30 @@ class Program
     public static void Main(string[] args)
     {
 
-        Caballo Friezan = new Caballo();
-        Humano Juan = new Humano();
-        Gorila Copito = new Gorila();
+        Caballo Friezan = new Caballo("Spirit");
+        Humano Juan = new Humano("Juan");
+        Gorila Copito = new Gorila("Copito");
 
-        
-        Console.WriteLine();
+        Juan.getNombre();
+        Friezan.getNombre();
+        Copito.getNombre();
 
     }
 }
 
 class Mamiferos
 {
+
+    //creando un campo de clase
+    private string nombreSerVivo;
+
+    //creando un constructor por defecto para la clase Mamiferos
+    public Mamiferos(String nombre)
+    {
+        nombreSerVivo = nombre;
+    }
+    
+
     public void Respirar()
         {
             Console.WriteLine("Soy capaz de respirar");
@@ -27,28 +39,51 @@ class Mamiferos
             Console.WriteLine("Cuido de mis crías hasta que se puedan valer por sí mismas");
         }
 
+    public void getNombre()
+        {
+            Console.WriteLine("El nombre del ser vivo es: " + nombreSerVivo);
+        }
+
      }
 
-    class Caballo : Mamiferos
+class Caballo : Mamiferos
     {
+
+        public Caballo(String nombreCaballo) : base(nombreCaballo)
+        {
+
+        }
+
         public void Galopar()
         {
             System.Console.WriteLine("Soy capaz de galopar");
         }
      }
 
-    class Humano : Mamiferos
-     {
+class Humano : Mamiferos
+{
+
+        public Humano(string nombreHumano) : base(nombreHumano)
+        {
+
+        }
         public void Pensar()
         {
             System.Console.WriteLine("Soy capaz de pensar ¿?");
         }
      }
 
-    class Gorila : Mamiferos
-     {
-        public void Trepar()
-        {
-            Console.WriteLine("Soy capaz de trepar");
-        }
-     }
+class Gorila : Mamiferos
+{
+    
+    public Gorila(String nombreGorila) : base(nombreGorila)
+    {
+
+    }
+
+    public void Trepar()
+    {
+        Console.WriteLine("Soy capaz de trepar");
+    }
+
+}
