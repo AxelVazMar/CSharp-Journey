91a5a6e3a583786080c5c72ac0bf5533225edf57
Axel Vazquez <axelvazmar0@gmail.com>
2024-10-14 09:52:47 -0600

    Conociendo el principio de sustitución ES SIEMPRE UN

--- Herencia/Program.cs
+++ Herencia/Program.cs
@@ -9,9 +9,50 @@ class Program
         Humano Juan = new Humano("Juan");
         Gorila Copito = new Gorila("Copito");
 
-        Juan.getNombre();
+        /* Creando arrays con el principio de sutitución */
+
+        Caballo[] almacenAnimales = new Caballo[3];
+        almacenAnimales[0] = Friezan;
+        //almacenAnimales[1] = Juan; // En este caso el array da error ya que el array que estamos creando es de tipo caballo
+        //Por lo que no se pueden alamacernar datos de otro tipo que no sea de tipo Caballo
+
+        Mamiferos[] almacenMamiferos = new Mamiferos[3];
+        almacenMamiferos[0] = Friezan;
+        almacenMamiferos[1] = Juan;
+        almacenMamiferos[2] = Copito;
+        /* Este array no da error ya que estamos usando el principio de sustitución ya que el array es de tipo mamiferos
+        por lo que será capaz de almacenar datos de tipo Mamiferos */
+
+        almacenMamiferos[1].getNombre();
+        for (int i = 0; i < almacenMamiferos.Length; i++)
+        {
+            System.Console.WriteLine(almacenMamiferos[i]);
+        }
+
+        /*Juan.getNombre();
         Friezan.getNombre();
-        Copito.getNombre();
+        Copito.getNombre();*/
+
+        /* Principio de sustitución "ES SIEMPRE UN" */
+        //Caballo animal = new Mamiferosa("Bucéfalo"); Caso ERRONEO ya que un Mamífero no es siempre un caballo
+        //Mamiferos animal = new Caballo("Bucéfalo"); // Caso correcto ya que un Caballo es siempre un Mamífero
+        //Mamiferos persona = new Humano("Axel"); // Otro caso correcto ya que una Persona es siempre un Mamífero aunque
+        // la clase "Humano" no existe sí que deja usar las propiades generales de la Clase cósmica "Mamiferos"
+
+        /* Instancias donde no usamos el principio de sustitución */
+        Mamiferos animal = new Mamiferos("Bucéfalo");
+        Caballo Bucefalo = new Caballo("Bucéfalo");
+        /* Solo hemos creado instancias pertenecientes a las clases Mamiferos y Caballo */
+
+        /* Aquí sí usamos el principio de sustitución a partir de dos instancias previamente creadas */
+        animal = Bucefalo; 
+        /* Aquí es válido ya que la variable animal pertenece a la clase mamiferos y 
+        la variable Bucefalo pertemece a la clase caballo 
+        Por lo que un Caballo es siempre un Mamifero */
+
+        // Bucefalo = animal; Aquí la sintaxis es incorrecta ya que estaríamos diciendo que un Mamifero es siempre un Caballo
+
+        Object miAnimal = new Caballo("Bucefalo");
 
     }
 }
--- Herencia/obj/Debug/net8.0/Herencia.AssemblyInfo.cs
+++ Herencia/obj/Debug/net8.0/Herencia.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Herencia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e7713db869bdad30f292ef9cf6bd5c1e34a0fcd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6063c067a49124efa96e58d0687466211d480ee0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Herencia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Herencia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
