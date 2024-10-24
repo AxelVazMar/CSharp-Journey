33cae4061f1f3da32d807a4949f9fa79e435b3e4
Axel Vazquez <axelvazmar0@gmail.com>
2024-10-24 07:22:32 -0600

    Conociendo las Structs vs las Clases en C#

---
+++ Structs y Enum/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Empleado empleado1 = new Empleado(1200, 250);
+        empleado1.CambiaSalario(empleado1, 100);
+        Console.WriteLine(empleado1);
+    }
+}
+
+public struct Empleado
+{
+    public double salarioBase, comision;
+
+    public Empleado(int salarioBase, int comision)
+    {
+        this.salarioBase = salarioBase;
+        this.comision = comision;
+    }
+
+    public override string ToString()
+    {
+        return string.Format("Salario y comisón del empleado: {0},{1}", this.salarioBase, this.comision);
+    }
+
+    public void CambiaSalario(Empleado emp, double incremento)
+    {
+        emp.salarioBase += incremento;
+        emp.comision += incremento;
+    }
+}
