31396621d3bab4692e69a2ec19b0dfd1e2b4b785
Axel Vazquez <axelvazmar0@gmail.com>
2024-10-24 08:54:58 -0600

    Conociendo los métodos destructores para el manejo de memoria

---
+++ Destructores/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+
+namespace Destructores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ManejoArchivos miArchivo = new ManejoArchivos();
+            miArchivo.Mensaje();
+        }
+    }
+
+    class ManejoArchivos
+    {
+        StreamReader archivo = null;
+        int contandor = 0;
+        string linea;
+
+        public ManejoArchivos()
+        {
+            archivo = new StreamReader(@"/Users/axelvm/Documents/C#/CSharp-Journey/CSharp-Journey/hola.txt"); // abriendo el archivo
+            while((linea = archivo.ReadLine()) != null)
+            {
+                Console.WriteLine(linea);
+                contandor++;
+            }
+        }
+
+        public void Mensaje()
+        {
+            Console.WriteLine("El archivo tiene {0} líneas", contandor);
+            
+        }
+
+        /* CONSTRUYENDO UN MÉTODO DESTRUCTOR */
+        ~ManejoArchivos()
+        {
+            archivo.Close(); // Aquí se cerraría el flujo de de datos del archivo o lo que es igual, deja de leer el archivo
+        }
+    }
+}
