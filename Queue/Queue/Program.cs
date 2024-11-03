85f9297bb3bd4c4dd6c5f24338506f2b7069e03b
Axel Vazquez <axelvazmar0@gmail.com>
2024-11-03 12:08:21 -0600

    Conociendo las colas (queue) en C#

---
+++ Queue/Queue/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> numeros = new Queue<int>();
+            // Rellenando o agregando elementos en la cola
+            foreach (int numero in new int[5] { 2, 4, 6, 8, 10 })
+            {
+                numeros.Enqueue(numero);
+            }
+
+            // Recorriendo la cola
+            Console.WriteLine("\nRecorriendo la QUEUE");
+            foreach (int numero in numeros)
+            {
+                Console.WriteLine(numero);
+            }
+            
+            // Eliminando elementos de la cola
+            Console.WriteLine("\nEliminando elementos de la Queue");
+            numeros.Dequeue();
+            foreach (int numero in numeros)
+            {
+                Console.WriteLine(numero);
+            }
+        }
+    }
+}
